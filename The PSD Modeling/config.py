728f741f9a6e9812290d9199596bbc18cb20292b
Sara <56139073+sarashahin@users.noreply.github.com>
2025-03-04 15:09:12 +0000

    Add files via upload

--- The PSD Modeling/config.py
+++ The PSD Modeling/config.py
@@ -20,20 +20,20 @@ INV = 1e-8
 # Mortality rate (for large negative growth offsets)
 MORTALITY_RATE = 0.2
 
-# Probability of non-zero interaction
-CONNECTANCE = 0.4
-
-# Strength of interaction when non-zero
-INTERACTION_STRENGTH = 0.4
-
 #: Initial number of species to aim for before the main loop
 TARGET_RICHNESS = 300
 
 #: Number of time steps for "relaxation" or "settling" in certain procedures
 T_RELAX = 300
 
+# Probability of non-zero interaction
+CONNECTANCE = 0.4
+
+# Strength of interaction when non-zero
+INTERACTION_STRENGTH = 0.4
+
 #: Max simulation time
-TMAX =  18000  # (like 200000 in R)
+TMAX =  200000  # (like 200000 in R)
 
 #: Step size per iteration in the simpler discrete loops
 STEP_SIZE = 1
@@ -57,3 +57,14 @@ THRESHOLD = 10 * BODY_MASS
 
 # We also define maximum steps for Assimulo
 MAX_STEPS = 100000
+
+# -----------------------------
+# Consumer Parameters
+# -----------------------------
+NUM_CONSUMERS = 1
+TOTAL_SPECIES = TARGET_RICHNESS + NUM_CONSUMERS
+
+ATTACK_RATE = 0.3             # Base attack rate for consumer on resources
+SIGMA = 0.6                  # Sigma parameter for consumer dietary breadth
+CONVERSION_EFFICIENCY = 0.1    # Conversion efficiency for consumer biomass gain
+RESPIRATION_RATE = 0.1         # Respiration rate for consumers
