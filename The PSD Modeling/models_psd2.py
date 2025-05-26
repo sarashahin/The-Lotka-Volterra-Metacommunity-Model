9c7470adbe098dc8291c89303a58bd3557f268ba
Sara <56139073+sarashahin@users.noreply.github.com>
2025-05-26 19:43:13 +0100

    Update models_psd2.py

--- The PSD Modeling/models_psd2.py
+++ The PSD Modeling/models_psd2.py
@@ -1,18 +1,24 @@
 
 ############################################
-# models_psd2.py
+# models_PSD2.py
 ############################################
 
 """
-PSD2 approach with 'chunked' assimilation, iterative solver to reduce memory usage,
+PSD approach with 'chunked' assimilation, iterative solver to reduce memory usage,
 and more frequent logging to avoid silent long steps that risk OS kills.
 """
 
 import sys
 import numpy as np
 import logging
-from assimulo.solvers import CVode
+from assimulo.solvers import CVode, ExplicitEuler
 from assimulo.problem import Explicit_Problem
+from euler_simple import EulerSimple
+from config import (
+    BODY_MASS,
+    MORTALITY_RATE,
+    STEP_SIZE,
+    TMAX, INV, RTOL, ATOL)
 
 logger = logging.getLogger(__name__)
 
@@ -36,7 +42,7 @@ class PSD2Model:
         self.poisson_clock = np.log(np.random.rand(self.S))
 
         # For storing results
-        self.nrecords = max(1, self.tmax // self.record_step)
+        self.nrecords = int(max(1, self.tmax // self.record_step))
         self.trajectory = np.zeros((self.nrecords + 1, self.S))
         self.wait_trajectory = np.zeros((self.nrecords + 1, self.S), dtype=bool)
         self.time_points = np.zeros(self.nrecords + 1)
@@ -76,29 +82,21 @@ class PSD2Model:
         return flat
 
     def _derivatives(self, t, y, sw):
-        y = self._ensure_flat_y(y) # looks rather expensive to call here
+        #y = self._ensure_flat_y(y) # looks rather expensive to call here
         logB = y[:self.S]
-        pclock = y[self.S:2*self.S]
 
-        B = np.exp(logB) * np.invert(sw)
+        B = np.exp(logB)
         local_growth = self.r - self.C.dot(B)
 
-        dlogB = np.zeros(self.S)
-        dpclock = np.zeros(self.S) ## Should not be needed
+        diagB = np.diag(self.C)*B
+        dlogB = local_growth + INV/B - 2*np.array(sw)*(local_growth+diagB)
+        non_self_growth = local_growth + diagB
+        non_self_growth[np.invert(sw)] = 0
+        non_self_growth[non_self_growth < 0] = 0
+        denom = non_self_growth + MORTALITY_RATE
+        est_prob = non_self_growth / denom
+        dpclock  =  est_prob * (INV / BODY_MASS)
 
-        for i in range(self.S):
-            if not sw[i]:
-                # Non-waiting => normal logB derivative
-                dlogB[i] = local_growth[i] + np.exp(np.log(INV) - logB[i])
-            # Run pclock even if not waiting, so that waiting <-> pclock < 0
-            non_self_growth = local_growth[i] + B[i]*self.C[i,i]
-            denom = non_self_growth + MORTALITY_RATE
-            if non_self_growth >= 0: # implies denom > 0 
-                est_prob = non_self_growth / denom
-            else:
-                est_prob = 0.0
-            dpclock[i] = INV * est_prob / BODY_MASS
-        
         return np.concatenate([dlogB, dpclock])
 
     def _event_fn(self, t, y, sw):
@@ -107,20 +105,21 @@ class PSD2Model:
           event(2*i)   = local_growth[i]
           event(2*i+1) = pclock[i]
         """
-        y = self._ensure_flat_y(y) # looks rather expensive to call here
+        #y = self._ensure_flat_y(y) # looks rather expensive to call here
         logB = y[:self.S]
         pclock = y[self.S:2*self.S]
 
-        B = np.exp(logB) * np.invert(sw)
+        B = np.exp(logB)
+        
         local_growth = self.r - self.C.dot(B)
 
         # Remove effect of intraspecific competition
         local_growth = local_growth + np.diag(self.C) * B 
         return np.concatenate([local_growth, pclock])
 
-    def _handle_event_fn(self, solver, event_info):
+    def _handle_event_fn(self, solver, event_info): #(self, t, y):
         """Event logic by sign changes in _event_fn."""
-        y = self._ensure_flat_y(solver.y) # perhaps not needed?
+        y = solver.y # self._ensure_flat_y(solver.y)
         logB = y[:self.S].copy()
         pclock = y[self.S:2*self.S].copy()
 
@@ -131,7 +130,8 @@ class PSD2Model:
         state_info = event_info[0]
         changed = np.nonzero(state_info)[0]
 
-        B = np.exp(logB) * np.invert(solver.sw)
+        B = np.exp(logB)
+        
         local_growth = self.r - self.C.dot(B)
         
         # Remove effect of intraspecific competition
@@ -146,13 +146,30 @@ class PSD2Model:
                     else:
                         print(f"{i_species} S ({local_growth[i_species]}) -> P at {solver.t}")
                         solver.sw[i_species] = False
-                        solver.y[i_species] = np.log(INV/100.0)
-                        solver.y[i_species+self.S] = 1
+                        solver.y[i_species+self.S] = 1 # make pclock > 0 if not waiting
                 elif state_info[idx] == +1:  # species was not waiting, goes to waiting
-                    print(f"{i_species} P ({local_growth[i_species]}) -> S at {solver.t}")
-                    solver.sw[i_species] = True
-                    solver.y[i_species+self.S] = np.log(np.random.rand())
-                    solver.y[i_species] = np.log(INV/100.0) ## should not be needed
+                    # Compute rate of change c of intrinsic growth rate of i_species
+                    sw_fixed = solver.sw.copy()
+                    #sw_fixed[i_species] = True
+                    yd = self._derivatives(solver.t,solver.y,sw_fixed)
+                    yd[i_species] = 0 # Remove effect of intraspecific competition
+                    c = -sum(self.C[i_species,:]*B*yd[:self.S])
+                    print(f"c = {c}")
+                    if c <= 0:
+                        print(f"{i_species} Sweep {c} is not positive!")
+                        print(f"Try reducing 'inith' parameter.")
+                        transition_to_S = True
+                    else:
+                        prob_to_S = np.exp(-B[i_species]/(BODY_MASS*(1+np.sqrt(np.pi/(2*c))*MORTALITY_RATE)))
+                        print(f"prob_to_S = {prob_to_S}")
+                        transition_to_S = (np.random.rand(1) <= prob_to_S)
+                    if not transition_to_S:
+                        print(f"{i_species} P ({local_growth[i_species]}) -> D at {solver.t}")
+                        solver.y[i_species] = min(0,solver.y[i_species] - np.log(1-prob_to_S))
+                    if transition_to_S:
+                        print(f"{i_species} P ({local_growth[i_species]}) -> S at {solver.t}")
+                        solver.sw[i_species] = True
+                        solver.y[i_species+self.S] = np.log(np.random.rand())
                 else:
                     print(f"{i_species} D -> P at {solver.t}")
             else: # event with pclock crossing zero => establishment
@@ -164,15 +181,15 @@ class PSD2Model:
                     denom = local_growth[i_species] + MORTALITY_RATE
                     if local_growth[i_species] >= 0: # implies denom > 0 
                         est_prob = local_growth[i_species] / denom
+                        val = BODY_MASS / est_prob if est_prob > 0 else BODY_MASS
+                        if val > 1: ## avoid values that are too large
+                            solver.y[i_species] = 0.0
+                        else:
+                            solver.y[i_species] = np.log(val)
+                        solver.y[i_species + self.S] = 1 ## pclock unused
+                        solver.sw[i_species] = False
                     else:
                         print("Local growth negative when pclock triggered!")
-                        sys.exit()
-                    val = BODY_MASS / est_prob if est_prob > 0 else BODY_MASS
-                    if val > 1: ## avoid values that are too large
-                        solver.y[i_species] = 0.0
-                    else:
-                        solver.y[i_species] = np.log(val)
-                    solver.sw[i_species] = False
         
     def run(self):
         logger.info("Starting PSD2 simulation with Assimulo...")
@@ -187,20 +204,27 @@ class PSD2Model:
         problem.handle_event = self._handle_event_fn
         problem.number_of_state_events = 2*self.S
 
-        # Solver configuration
-        solver = CVode(problem)
-        solver.discr = 'BDF'
-        solver.iter = 'Newton'
-        # For large S, "Dense" can be huge. Use "SPGMR" for an iterative approach.
-        solver.linear_solver = 'SPGMR'
-        solver.rtol = 1e-5  # Looser tolerance for big system
-        solver.atol = 1e-4
-
-        solver.options['hmin'] = 1e-4
-        solver.options['maxh'] = 1000
-        solver.options['root_tol'] = 1e-1
-        solver.options["mxhnil"] = 5
-        solver.options['maxsteps'] = 300
+        if True:
+            # Solver configuration
+            solver = CVode(problem)
+            solver.discr = 'BDF'
+            solver.iter = 'Newton'
+            # For large S, "Dense" can be huge. Use "SPGMR" for an iterative approach.
+            solver.linear_solver = 'SPGMR'
+            solver.rtol = 0  # Looser tolerance for big system
+            solver.atol = 1e-3
+            solver.inith = 1e-7 # large values cause repeat evaluation of conditions
+            solver.maxh = 1e10
+            solver.store_event_points = False
+            solver.options["mxhnil"] = 5
+            solver.options['maxsteps'] = 10000
+            solver.options['verbosity'] = 30 # QUIET = 50 WHISPER = 40 NORMAL = 30 LOUD = 20 SCREAM = 10
+        else:
+            solver = EulerSimple(problem)
+            solver.options['inith'] = 1
+            solver.options['maxsteps'] = 10000000
+            solver.store_event_points = False
+            
 
         # #### TESTING ####
         # self._derivatives(0, y0, solver.sw)
@@ -209,6 +233,11 @@ class PSD2Model:
         # print("TESTING OK")
         # sys.exit()
         
+        # Chunk the integration to avoid huge steps
+        chunk_size = 2000
+        times = np.arange(0, self.tmax + chunk_size, chunk_size)
+        times = np.unique(np.clip(times, 0, self.tmax))
+
         # Recording times (for output snapshots)
         record_times = np.arange(0, self.tmax + self.record_step, self.record_step)
         record_times = np.unique(np.clip(record_times, 0, self.tmax))
@@ -216,17 +245,14 @@ class PSD2Model:
         # Run the simulation
         t, y = solver(self.tmax, record_times.shape[0]-1)
 
-        recSteps = np.where(np.remainder(t,self.record_step) == 0)[0]
-        if recSteps.shape[0] != record_times.shape[0]:
-            sys.exit("Could not localise recording time steps after simulation.")
-
-        for step in range(recSteps.shape[0]):
-            pclock = y[recSteps[step],self.S:(2*self.S)]
+        for step in range(record_times.shape[0]):
+            recStep = np.argmin(np.abs(t - record_times[step]))
+            pclock = y[recStep,self.S:(2*self.S)]
             waiting = pclock < 0
-            B = np.exp(y[recSteps[step],0:self.S]) * np.invert(waiting)
+            B = np.exp(y[recStep,:self.S])
             self.trajectory[step, :] = B
             self.wait_trajectory[step, :] = waiting
-            rec_time = t[recSteps[step]]
+            rec_time = t[recStep]
             self.time_points[step] = rec_time
             # Compute extra diagnostics at t = 0
             growth = self.r - self.C.dot(B)
