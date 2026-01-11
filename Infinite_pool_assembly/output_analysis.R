bff82579734ffceb28afef98952b9ad5f2a9f404
Axel G. Rossberg <axel@rossberg.net>
2026-01-11 11:13:52 +0900

    Updated analyses

--- Infinite_pool_assembly/output_analysis.R
+++ Infinite_pool_assembly/output_analysis.R
@@ -33,20 +33,36 @@ B_ibm_history <- B_ibm_history[(length(B_ibm_history)/2):length(B_ibm_history)]
 hist(log10(B_ibm_history),probability = T,col=rgb(0,1,0,0.2),add=T,border = "green")
 
 ##########################################################################
+library(extraDistr)
+library(pracma)
+
+x <- extraDistr::rlgser(5000, 0.13)
+mean_to_p <- function(x_bar){
+  xi <- -x_bar*pracma::lambertWn(-exp(-1/x_bar)/x_bar)-1
+  xi/(xi+1)
+}
+
+var_for_p <- function(p){
+  -(p^2 + p*log(1-p))/((1-p)*log(1-p))^2
+}
 
 occupancy <- read.csv("occupancy.csv")
 nrow(occupancy)
 sum(occupancy$type==1)
 mean(occupancy$type==1)
-h <- hist(occupancy$occupancy[occupancy$type==1],breaks = seq(0,400),plot = F)
-plot(h$mids,h$counts+0.1,xlim=c(1,200),log="y")
 mean(occupancy$occupancy[occupancy$type==1])
+p1 <- mean_to_p(mean(occupancy$occupancy[occupancy$type==1])); p1
+var(occupancy$occupancy[occupancy$type==1])/var_for_p(p1)
+h <- hist(occupancy$occupancy[occupancy$type==1],breaks = seq(0,2000),plot = F)
+plot(h$mids,h$counts,log="y")
 
 sum(occupancy$type==2)
 mean(occupancy$type==2)
-h <- hist(occupancy$occupancy[occupancy$type==2],breaks = seq(0,400),plot = F)
-plot(h$mids,h$counts,xlim=c(1,50),log="y")
 mean(occupancy$occupancy[occupancy$type==2])
+p2 <- mean_to_p(mean(occupancy$occupancy[occupancy$type==2])); p2
+var(occupancy$occupancy[occupancy$type==2])/var_for_p(p2)
+h <- hist(occupancy$occupancy[occupancy$type==2],breaks = seq(0,2000),plot = F)
+plot(h$mids,h$counts,xlim=c(1,600),log="y")
 
 mean(occupancy$occupancy)
 
@@ -54,6 +70,14 @@ sum(occupancy$occupancy)
 
 mean(occupancy$occupancy[occupancy$type==1])*sum(occupancy$type==1)
 mean(occupancy$occupancy[occupancy$type==2])*sum(occupancy$type==2)
-
+sum(occupancy$occupancy)
 
 ###########################################################################
+
+
+nmax <- 50*12
+cutIt <- function(n){ifelse(n > nmax, nmax, n)}
+mean(cutIt(rlgser(30,theta =0.9987)))
+var(cutIt(rlgser(30,theta =0.9987)))/var(rlgser(30,theta =0.9987))
+
+
