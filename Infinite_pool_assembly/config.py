28224435304ed565134c81ae8da91cd257df637d
Axel G. Rossberg <axel@rossberg.net>
2026-01-11 10:52:20 +0900

    50 x 50 grid, more localised dispersal

--- Infinite_pool_assembly/config.py
+++ Infinite_pool_assembly/config.py
@@ -50,8 +50,8 @@ THRESHOLD = 10 * BODY_MASS
 # -----------------------------
 # Spatial (Multi-patch) Parameters
 # -----------------------------
-NUM_PATCHES_X = 20
-NUM_PATCHES_Y = 20
+NUM_PATCHES_X = 50
+NUM_PATCHES_Y = 50
 
 # Dispersal rate (diffusion coefficient)
 DISPERSAL_RATE = BODY_MASS * 1e-3 
@@ -72,6 +72,6 @@ LOG_B_CAP        = math.log(ECOLOGICAL_MAX_B)
 # Example (Gaussian): 
 # DISPERSAL_KERNEL = lambda r: np.exp(-r**2 / (2 * 2.0**2))
 DISPERAL_A = 1
-DISPERAL_B = 2.5  # 1.25
+DISPERAL_B = 1.25  # 1.25
 DISPERSAL_KERNEL = lambda r: (1 + (r/DISPERAL_A)**2)**(-DISPERAL_B)
 
